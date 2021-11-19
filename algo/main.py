ab4bef4c4ba69cb69f85fdf2d71ab03d04f23446
Phat Thaveepholcharoen <overtone00445@gmail.com>
2021-11-20 01:29:22 +0700

    Add test case and update main.py

--- algo/main.py
+++ algo/main.py
@@ -2,14 +2,13 @@ from solve import solve
 
 
 def main():
-    r1 = solve(["ab", "bc"], "abcd")  # ("ab", "cd")
-    print(r1)
+    r1 = solve(["ab", "bc", "cd"], "abcd")
+    r2 = solve(["ab", "bc", "cd"], "cdab")
+    r3 = solve(["ab", "bc", "cd"], "abab")
 
-    r2 = solve(["ab", "bc", "cd"], "cdab")  # ("cd", "ab")
-    print(r2)
-
-    r3 = solve(["ab", "bc", "cd"], "abab")  # None
-    print(r3)
+    print("r1", r1)
+    print("r2", r2)
+    print("r3", r3)
 
 
 main()
--- algo/test.py
+++ algo/test.py
@@ -25,6 +25,22 @@ def test_solve_case6():
     assert solve(["ab", "bc"], "abbc") == ("ab", "bc")
 
 
+def test_solve_case7():
+    assert solve(["ab"], "abbc") == None
+
+
+def test_solve_case8():
+    assert solve(["ab", "bb", "bc"], "bbbc") == ("bb", "bc")
+
+
+def test_solve_case9():
+    assert solve([""], "abbc") == None
+
+
+def test_solve_case10():
+    assert solve([], "") == None
+
+
 if __name__ == "__main__":
     test_solve_case1()
     test_solve_case2()
@@ -32,5 +48,9 @@ if __name__ == "__main__":
     test_solve_case4()
     test_solve_case5()
     test_solve_case6()
+    test_solve_case7()
+    test_solve_case8()
+    test_solve_case9()
+    test_solve_case10()
 
     print("Everything working fine")
