5308465bca43940d431f4e979513b636c53b6116
Phat Thaveepholcharoen <overtone00445@gmail.com>
2021-11-20 23:34:10 +0700

    Add example for connecting MetaMask

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Web3ReactProvider } from "@web3-react/core";
+import React from "react";
+import Web3 from "web3";
+import "./App.css";
+import Home from "./pages/Home";
+
+function getLibrary(provider: any) {
+  return new Web3(provider);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Home />
+    </Web3ReactProvider>
   );
 }
 
