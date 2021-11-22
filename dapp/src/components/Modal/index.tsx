5dfbec540b78a747ceb6102d49fb35e903b86ceb
Phat Thaveepholcharoen <overtone00445@gmail.com>
2021-11-22 22:48:22 +0700

    Add mounted animation to modal

--- dapp/src/components/Modal/index.tsx
+++ dapp/src/components/Modal/index.tsx
@@ -2,6 +2,7 @@ import React, { ComponentPropsWithoutRef } from "react";
 import ReactDOM from "react-dom";
 
 import { ModalContent, ModalOverlay, ModalWrapper } from "./styled";
+import { animated, useTransition } from "react-spring";
 
 interface Props extends ComponentPropsWithoutRef<"div"> {
   isShowing: boolean;
@@ -9,15 +10,32 @@ interface Props extends ComponentPropsWithoutRef<"div"> {
 }
 
 export function Modal({ isShowing, hide, children }: Props) {
-  return isShowing
-    ? ReactDOM.createPortal(
-        <React.Fragment>
-          <ModalOverlay />
-          <ModalWrapper aria-modal aria-hidden tabIndex={-1} role="dialog">
-            <ModalContent>{children}</ModalContent>
-          </ModalWrapper>
-        </React.Fragment>,
-        document.body
-      )
-    : null;
+  const transitionAnim = useTransition(isShowing, {
+    from: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+    },
+    leave: {
+      opacity: 0,
+    },
+  });
+
+  return ReactDOM.createPortal(
+    <React.Fragment>
+      {transitionAnim(
+        (style, item) =>
+          item && (
+            <animated.div style={style}>
+              <ModalOverlay />
+              <ModalWrapper aria-modal aria-hidden tabIndex={-1} role="dialog">
+                <ModalContent>{children}</ModalContent>
+              </ModalWrapper>
+            </animated.div>
+          )
+      )}
+    </React.Fragment>,
+    document.body
+  );
 }
